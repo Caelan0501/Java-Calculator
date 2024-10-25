9351694862122eb1afa0f0a2f5c6c40a48395e08
Caelan Desmond <caelan0501@gmail.com>
2024-10-25 03:53:36 -0700

    Working Basic Desktop Calculator app

--- DesktopGUI.java
+++ DesktopGUI.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-import calculator.*;
+import calculator.Arithmetic;
 
 public class DesktopGUI extends JFrame
 {
     private JPanel contentPane;
     private JButton Clear;
-    private JButton button1;
     private JButton one;
     private JButton negation;
     private JButton two;
@@ -29,18 +29,46 @@ public class DesktopGUI extends JFrame
     private JButton Subtract;
     private JButton Multiply;
     private JButton Divide;
-    private JButton ln;
-    private JButton ABS;
-    private JButton CE;
-    private JButton PiButton;
     private JButton Root;
     private JButton Power;
-    private JButton log;
+    private JTabbedPane Tabs;
+    private JPanel StandardTab;
 
     public DesktopGUI()
     {
+        decimal.addActionListener(this::AddText);
+        zero.addActionListener(this::AddText);
+        one.addActionListener(this::AddText);
+        two.addActionListener(this::AddText);
+        three.addActionListener(this::AddText);
+        four.addActionListener(this::AddText);
+        five.addActionListener(this::AddText);
+        six.addActionListener(this::AddText);
+        seven.addActionListener(this::AddText);
+        eight.addActionListener(this::AddText);
+        nine.addActionListener(this::AddText);
 
+        Add.addActionListener(this::AddText);
+        Subtract.addActionListener(this::AddText);
+        Multiply.addActionListener(this::AddText);
+        Divide.addActionListener(this::AddText);
+        Mod.addActionListener(this::AddText);
+        Power.addActionListener(this::AddText);
+        Root.addActionListener(this::AddText);
+        LPAREN.addActionListener(this::AddText);
+        RPAREN.addActionListener(this::AddText);
+        negation.addActionListener(this::AddText);
+
+        backButton.addActionListener(_ -> TextField.setText(TextField.getText().substring(0, TextField.getText().length() - 1)));
+        Clear.addActionListener(_ -> TextField.setText(""));
+
+        Equal.addActionListener(_ -> {
+            try { TextField.setText(Double.toString(Arithmetic.Solve(TextField.getText()))); }
+            catch (Exception ex) { TextField.setText("Error"); }
+        });
     }
+
+    public void AddText(ActionEvent e) { TextField.setText(TextField.getText() + ((JButton) e.getSource()).getText()); }
 
     public static void main(String[] args)
     {
